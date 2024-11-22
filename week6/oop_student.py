8b54451a7a2bbd04b8ef2a66317b49a528c17083
hnuengtida-tnk <67319010060@tnk.ac.th>
2024-11-22 16:37:49 +0700

    หนึ่งธิดา

--- week6/oop_student.py
+++ week6/oop_student.py
@@ -5,12 +5,18 @@ class Student:
         self.name = ชื่อสกุล
         self.nickname = ชื่อเล่น
         self.score = random.randint(1,10) 
+        self.editscore = random.randint(1,10)
 
     def scores(self):
         if self.score >= 5:
             print(f"ชื่อสกุล : {self.name}, ชื่อเล่น : {self.nickname}, คะแนน : {self.score} : คุณผ่าน")
         else :
             print(f"ชื่อสกุล : {self.name}, ชื่อเล่น : {self.nickname}, คะแนน : {self.score} : คุณตก")
+             print("--------------------สอบแก้---------------------------")
+        if self.editscore >= 5:
+            print(f"ชื่อ-นามสกุล : {self.name} , ชื่อเล่น : {self.nikename}, คะแนน : {self.editscore } : คุณผ่าน")
+        else :
+            print(f"ชื่อ-นามสกุล : {self.name} , ชื่อเล่น : {self.nikename}, คะแนน : {self.editscore } : คุณตก")
 
 Student1 = Student("หนึ่งธิดา อินทรชัย","สตางค์")
 Student2 = Student("วิไลวรรณ พลเดช","พลอย")
